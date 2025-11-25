64f82dc7ec164671db5afd76c091c239cbcda1ab
William Benz <226767563+WilliamBenz1337@users.noreply.github.com>
2025-11-25 14:56:40 +0000

    Create Basic if statement.py

---
+++ Python/Basics/Basic if statement.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))               #Ask the user for their age    
+
+if age >= 20:                                      #If age greater or equals to than 20 then
+    print("You are allowed to drive a car!")       #then print this
+elif age >= 70:                                    #If age greater or equals to 70 then
+    print("You are a senior!")                     #Print this
+else:                                              #ANYTHING OUTSIDE OF THESE VALUES
+    print("Please try later!")                     #Print this
